d5cc0e4e73789e6ae60f2f076a3780fd0f85cae0
Shrikant-Meshram <mshrikantr99@gmail.com>
2022-05-06 11:19:18 +0530

    final Change

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -4,9 +4,9 @@ const validator = require('validator')
 
 const internModel = require("../models/internModel")
 
-let isValid= /\d/
+let isValid= /\d/           //regex for  validation
 
-let isValidLink = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+let isValidLink = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/  //regex use for validation of  logo link
 
 const createColleges = async function (req, res) {
     try {
@@ -15,26 +15,31 @@ const createColleges = async function (req, res) {
             return res.status(400).send({ status: false, message: "Invalid request parameters please provide Collage details" })
         }
         if (!data.name){
-            return res.status(400).send({ status: false, msg: " Please Provide  name" });
+            return res.status(400).send({ status: false, message: " Please Provide  name" });
         }
-        if(isValid.test(data.name)){
-            return res.status(400).send({status:false,msg:'Name Should not contain Numbers'})
+
+         //use here validation for name,fullName,logolink
+
+        if(isValid.test(data.name)){       
+            return res.status(400).send({status:false,message:'Name Should not contain Numbers'})
         }
         if (!data.fullName) {
-            return res.status(400).send({ status: false, msg: " Please Provide  fullName" });
+            return res.status(400).send({ status: false, message: " Please Provide  fullName" });
         }
         
+         //check weather string contain a number 
+
         if(isValid.test(data.fullName)){
-            return res.status(400).send({status:false,msg:'fullName Should not contain Numbers'})
+            return res.status(400).send({status:false,message:'fullName Should not contain Numbers'})
         }
 
 
         if (!data.logoLink) {
-            return res.status(400).send({ status: false, msg: " Please Provide  logolink" });
+            return res.status(400).send({ status: false, message: " Please Provide  logolink" });
         }
 
-        if(!isValidLink.test(data.logoLink)){
-            return res.status(400).send({status:false,msg:"Please enter valid logoLink"})
+        if(!isValidLink.test(data.logoLink)){      //logolink validation
+            return res.status(400).send({status:false,message:"Please enter valid logoLink"})
         }
         
 
@@ -53,16 +58,16 @@ const createColleges = async function (req, res) {
 
 const getCollegeDetails = async function(req,res){
     try{
-        let data =req.query.collegeName
+        let data =req.query.collegeName   //getting the data from query
 
-        if(!data) return res.status(400).send({status:true,msg:"please provide query"})
+        if(!data) return res.status(400).send({status:true,message:"please provide query"})
 
-        let saveId = await collegeModel.findOne({name:data,isDeleted:false}).select({name:1,fullName:1,logoLink:1})
-        if(!saveId) return res.status(404).send({status:false,msg:"college Not Found"})
+        let saveId = await collegeModel.findOne({name:data,isDeleted:false}).select({name:1,fullName:1,logoLink:1})    // find the college by the condition
+        if(!saveId) return res.status(404).send({status:false,message:"college Not Found"})
        
-       let intrested = await internModel.find({collegeId:saveId._id, isDeleted:false}).select({name:1,email:1,mobile:1})
+       let intrested = await internModel.find({collegeId:saveId._id, isDeleted:false}).select({name:1,email:1,mobile:1})  //find the college intern by condition
        if(intrested.length !==0) {
-             let info = {  
+             let info = {                 
                  name : saveId.name,
                fullName : saveId.fullName,
                logoLink : saveId.logoLink,
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -17,44 +17,42 @@ const createIntern = async function(req,res){
     }
 
     if(!data.name){
-        return res.status(400).send({ status: false, msg: "Please Provide name"})
+        return res.status(400).send({ status: false, message: "Please Provide name"})
 
     }
 
     if(isValid.test(data.name)){
-      return res.status(400).send({status:false,msg:'Name Should not contain Numbers'})
+      return res.status(400).send({status:false,message:'Name Should not contain Numbers'})
     }
     if (!data.email){
-      return res.status(400).send({ status: false, msg: " Please Provide email" });
+      return res.status(400).send({ status: false, message: " Please Provide email" });
     }
 
     const validEmail = validator.isEmail(data.email);
     if (validEmail === false){
-      return res.status(400).send({ status: false, msg: "Please enter valid email"})
+      return res.status(400).send({ status: false, message: "Please enter valid email"})
     }
 
 
     const checkEmail = await internModel.findOne({ email:data.email });
     if (checkEmail){
-      return res.status(409).send({ status: false, msg: "This email already exist"})
+      return res.status(400).send({ status: false, message: "This email already exist"})
     } 
-    if(data.mobile.length !== 10) return res.status(400).send({status:false ,msg:"enter valid mobile no"})
+    if(data.mobile.length !== 10) return res.status(400).send({status:false ,message:"enter valid mobile no"})
   
    
     if ( !isValidMobile.test(data.mobile)){
-      return res.status(400).send({ status: false, msg: "Mobile no. should be a digit"})
+      return res.status(400).send({ status: false, message: "Mobile no. should be a digit"})
     }
     let uniqueNo = await internModel.findOne({ mobile: data.mobile });
-     if(uniqueNo) return res.status(400).send({status:false ,msg:"mobileNo. already exist"})
+     if(uniqueNo) return res.status(400).send({status:false ,message:"mobileNo. already exist"})
 
     
-    // if(!data.collegeId){
-    //     return res.status(400).send({ status: false, msg: "Please Provide collegeId"})  
-    // }
+  
 
     let collegedata = await collegeModel.findOne({name:data.collegeName})
     if(!collegedata) {
-      return res.status(400).send({ status: false, msg: "Please Provide valid CollegeName" });
+      return res.status(400).send({ status: false, message: "Please Provide valid CollegeName" });
     }
      data["collegeId"] = collegedata._id
      delete data.collegeName
